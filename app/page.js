e3634c01c2bf491c8e79e3d0741de455bffa79db
김윤호 <dbsgh5034@naver.com>
2024-05-14 12:39:30 +0900

    Setting

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <span>가나다</span>
+        <span>Cool blue</span>
+      </div>
+    </main>
+  );
 }
