8eaa35634568d325be21aea0143fcd8fc6605b5c
김윤호 <dbsgh5034@naver.com>
2024-06-18 16:24:21 +0900

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   basePath: "/want-mortgage",
   assetPrefix: "/want-mortgage/",
+  output: {
+    // /out 디렉터리에 정적 파일을 생성합니다.
+    dir: "out",
+  },
 };
 
 export default nextConfig;
