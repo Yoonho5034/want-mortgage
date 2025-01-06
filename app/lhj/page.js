3c0278f395fa660a69f78e2960938e36281da5cf
김윤호 <dbsgh5034@naver.com>
2025-01-06 21:31:52 +0900

    lhj

---
+++ app/lhj/page.js
@@ -0,0 +1,18 @@
+"use client";
+import MainPage from "../MainPage";
+
+const page = () => {
+  const User = [
+    // 이름
+    "이홍주",
+    // 전화번호
+    "010-4140-6574",
+    // 상담사 등록번호
+    "10-00043371",
+    // 카카오톡 링크
+    "https://open.kakao.com/o/sbQ6788g",
+  ];
+  return <MainPage User={User} />;
+};
+
+export default page;
