8251ccd2e4356eb94900783e0073d0feb5038a72
김윤호 <dbsgh5034@naver.com>
2025-04-11 09:15:35 +0900

    fix : rate

--- app/MainPage.js
+++ app/MainPage.js
@@ -32,8 +32,8 @@ export default function Home({ User }) {
   ];
   // 금리 정보
   const Rate = [
-    { value: "Apt", rate: "연 4.27% ~ 5.91%" },
-    { value: "Villa", rate: "연 4.77% ~ 6.41%" },
+    { value: "Apt", rate: "연 4.27% ~ 5.74%" },
+    { value: "Villa", rate: "연 4.77% ~ 6.24%" },
     "( 2025년 4월 기준 )",
   ];
   const handleCall = () => {
