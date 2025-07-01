518d27280343cbe91b299c1d5fe317a4eb935936
김윤호 <dbsgh5034@naver.com>
2025-07-01 16:17:33 +0900

    7월 금리

--- app/MainPage.js
+++ app/MainPage.js
@@ -32,9 +32,9 @@ export default function Home({ User }) {
   ];
   // 금리 정보
   const Rate = [
-    { value: "Apt", rate: "연 4.13% ~ 6.08%" },
-    { value: "Villa", rate: "연 4.63% ~ 6.58%" },
-    "( 2025년 6월 기준 )",
+    { value: "Apt", rate: "연 4.23% ~ 5.98%" },
+    { value: "Villa", rate: "연 4.73% ~ 6.48%" },
+    "( 2025년 7월 기준 )",
   ];
   const handleCall = () => {
     window.location.href = `tel:${User?.[1]}`; // 전화번호를 여기에 입력하세요
